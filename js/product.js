0ef128e713a6c24240a881e3e2321b87f84d5ab2
Agilar Gumilar <88954314+Agilar011@users.noreply.github.com>
2022-10-31 17:53:10 +0700

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,37 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.addEventListener('click',()=>{
+    if (counter >= carouselImages.length - 1) return;     
+    carouselSlide.transition = "tranform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click',()=>{
+    if (counter <= 0) return;     
+    carouselSlide.transition = "tranform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend',()=>{
+    if (carouselImage[counter].id === 'lastClone') {
+        carouselSlide.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (carouselImage[counter].id === 'firstClone') {
+        carouselSlide.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
